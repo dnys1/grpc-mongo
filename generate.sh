a11039f355fd045b1e7045a4be3ff866b7665c1e
Dillon Nys <dnys1@asu.edu>
2020-07-07 15:58:16 -0700

    Migrate folder structure

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 protoc -I proto \
-    --go_out=server/model/blogpb \
+    --go_out=internal/model/blogpb \
     --go_opt=paths=source_relative \
-    --go-grpc_out=server/model/blogpb \
+    --go-grpc_out=internal/model/blogpb \
     --go-grpc_opt=paths=source_relative \
     proto/blog.proto
